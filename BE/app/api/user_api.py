43d8a1a557344e789b354d92e2c0521dc37a1f48
pranavgogate-21 <pranavgogate21@gmail.com>
2024-12-29 12:46:45 +0530

    added custom encoder to encode orm objects

--- BE/app/api/user_api.py
+++ BE/app/api/user_api.py
@@ -7,6 +7,7 @@ from app.database.user_db import USERDB
 from sqlalchemy.future import select
 
 from app.Models.user import User
+from app.utils.custom_encoder import ORJSONResponse
 
 router = APIRouter()
 
@@ -14,7 +15,8 @@ router = APIRouter()
 async def get_all_user(db: AsyncSession = Depends(get_db_session)):
     response = await db.execute(select(USERDB))
     users = response.scalars().all()
-    return users
+    users = [User.from_orm(user) for user in users]
+    return ORJSONResponse({"data": users}, status_code=status.HTTP_200_OK)
 
 @router.get("/user/{user_id}")
 async def get_user(user_id:str, db: AsyncSession = Depends(get_db_session)):
@@ -23,7 +25,8 @@ async def get_user(user_id:str, db: AsyncSession = Depends(get_db_session)):
         user = result.scalar_one_or_none()
         if user is None:
             ResponseBuilder.error("User Not found",status.HTTP_404_NOT_FOUND)
-        return user
+        user = User.from_orm(user)
+        return ORJSONResponse({"data":user}, status_code=status.HTTP_200_OK)
 
     except Exception as e:
         print(f"An exception occurred in get_user:{e}")
