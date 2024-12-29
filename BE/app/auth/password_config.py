10f6668c159a9ed9b56f33d7b8d4549a0ed30851
pranavgogate-21 <pranavgogate21@gmail.com>
2024-12-29 11:31:20 +0530

    removed print statements

--- BE/app/auth/password_config.py
+++ BE/app/auth/password_config.py
@@ -8,6 +8,3 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password:str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-
-# print(f"Hashed password:{hash_password("12345")}")
-print(f"result: {verify_password("12345","$2b$12$/T66hHyTHJCO56oPFAkbpuSMcEhLb7HNJkNapNZPpNuuo2Gl4d2XG")}")
