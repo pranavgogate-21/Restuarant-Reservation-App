ba4a8f18be804a53b99d6b8030f16497b695b7b9
pranavgogate-21 <pranavgogate21@gmail.com>
2024-12-30 20:59:51 +0530

    adding extra models userout and userpassword

--- BE/app/Models/user.py
+++ BE/app/Models/user.py
@@ -4,7 +4,6 @@ from app.database.user_db import USERDB
 
 
 class User(BaseModel):
-    id:str
     first_name:str
     last_name:str
     email:str
@@ -12,3 +11,13 @@ class User(BaseModel):
 
     class Config:
         from_attributes = True
+
+class UserOut(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    email: str | None = None
+    phone_number: str | None = None
+
+class UserPassword(BaseModel):
+    old_password: str
+    new_password: str
