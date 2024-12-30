0c1c6927de44e3907cdd11f9bd53509bac36e52a
pranavgogate-21 <pranavgogate21@gmail.com>
2024-12-30 11:12:31 +0530

    created token class for a new db table

--- BE/app/Models/token.py
+++ BE/app/Models/token.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel
 
-class Token(BaseModel):
+class TokenOut(BaseModel):
     access_token: str | None = None
     refresh_token: str | None = None
+
+    class Config:
+        from_attributes = True
---
+++ BE/app/database/token.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlmodel import SQLModel, Field, Relationship
+
+
+
+class Token(SQLModel,table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    access_token: str = Field(index=True)
+    refresh_token: str = Field(index=True)
+    user_id: str = Field(...,foreign_key="userdb.id", unique=True)
+
+    user: Optional["USERDB"] = Relationship(back_populates="tokens")
+
