663d2d4757464c23aa085526dbfb392232b21655
pranavgogate-21 <pranavgogate21@gmail.com>
2024-12-30 21:01:02 +0530

    added update delete routes

--- BE/app/api/user_api.py
+++ BE/app/api/user_api.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.Models.response_builder import ResponseBuilder
+from app.auth.jwt_handler import validate_jwt_token
 from app.database.db import get_db_session
 from app.database.user_db import USERDB
 from sqlalchemy.future import select
 
-from app.Models.user import User
+from app.Models.user import User, UserOut, UserPassword
+from app.service.user_service import UserService
 from app.utils.custom_encoder import ORJSONResponse
 
 router = APIRouter()
@@ -30,5 +32,46 @@ async def get_user(user_id:str, db: AsyncSession = Depends(get_db_session)):
 
     except Exception as e:
         print(f"An exception occurred in get_user:{e}")
+
+@router.put("/user/{user_id}")
+async def update_user(user_id: str, user: UserOut, db: AsyncSession = Depends(get_db_session)):
+    try:
+        print("From update_user.....")
+        user_service = UserService(db)
+        is_updated, data = await user_service.update_user_service(user, user_id)
+        if not is_updated:
+            ResponseBuilder.error(data, status.HTTP_404_NOT_FOUND)
+        user = User.from_orm(user)
+        return ORJSONResponse({"data": user}, status_code=status.HTTP_200_OK)
+
+    except Exception as e:
+        print(f"An exception occurred in update_user:{e}")
+
+@router.patch("/user/{user_id}")
+async def update_user_password(user_id: str, password: UserPassword, db:AsyncSession = Depends(get_db_session)):
+    try:
+        print("From update_user_password....")
+        user_service = UserService(db)
+        is_updated, data = await user_service.update_password_service(password, user_id)
+        if not is_updated:
+            ResponseBuilder.error(data, status.HTTP_404_NOT_FOUND)
+        return ResponseBuilder.success(data, status.HTTP_200_OK)
+
+    except Exception as e:
+        print(f"An exception occurred in update_user_password:{e}")
+
+@router.delete("/user/{user_id}")
+async def delete_user(user_id: str, db: AsyncSession = Depends(get_db_session)):
+    try:
+        print("From delete_user.....")
+        user_service = UserService(db)
+        is_deleted, data = await user_service.delete_user_service(user_id)
+        if not is_deleted:
+            ResponseBuilder.error(data, status.HTTP_404_NOT_FOUND)
+        data = User.from_orm(data)
+        return ORJSONResponse({"data": data}, status.HTTP_200_OK)
+    except Exception as e:
+        print(f"An exception occurred in delete_user:{e}")
+
 
 
--- BE/app/service/user_service.py
+++ BE/app/service/user_service.py
@@ -3,11 +3,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy import or_
 
-from app.auth.password_config import verify_password
+from app.Models.user import UserOut, UserPassword
+from app.auth.password_config import verify_password, hash_password
 from app.database.user_db import USERDB
 
 
-class UserService():
+class UserService:
 
      def __init__(self, db:AsyncSession):
          print("UserService instance created...")
@@ -30,3 +31,52 @@ class UserService():
 
          except Exception as e:
              print(f"An exception occurred in authenticate_user:{e}")
+
+     async def update_user_service(self, user: UserOut, user_id: str):
+         try:
+             print("From update_user_service....")
+             result = await self.db.execute(select(USERDB).where(USERDB.id == user_id))
+             user_obj = result.scalar_one_or_none()
+             if user_obj is None:
+                 return False, "User Not Found"
+             user_obj.first_name = user.first_name
+             user_obj.last_name = user.last_name
+             user_obj.email = user.email
+             user_obj.phone_number = user.phone_number
+             self.db.add(user_obj)
+             await self.db.commit()
+             await self.db.refresh(user_obj)
+             return True, user_obj
+         except Exception as e:
+             print(f"An exception occurred in update_user_service:{e}")
+
+     async def update_password_service(self, password: UserPassword, user_id: str):
+         try:
+             print("From update_password_service....")
+             old_password = password.old_password
+             new_password = password.new_password
+             result = await self.db.execute(select(USERDB).where(USERDB.id == user_id))
+             user = result.scalar_one_or_none()
+             if not verify_password(old_password, user.password):
+                 return False, "Incorrect Password"
+             new_password = hash_password(new_password)
+             user.password = new_password
+             self.db.add(user)
+             await self.db.commit()
+             await self.db.refresh(user)
+             return True, "Password change successful"
+         except Exception as e:
+             print(f"An exception occurred in update_password_service...:{e}")
+
+     async def delete_user_service(self, user_id: str):
+         try:
+             print("From delete_user_service....")
+             result = await self.db.execute(select(USERDB).where(USERDB.id == user_id))
+             user = result.scalar_one_or_none()
+             if user is None:
+                 return False, "User Not Found"
+             await self.db.delete(user)
+             await self.db.commit()
+             return True, user
+         except Exception as e:
+             print(f"An exception occurred in delete_user_service:{e}")
