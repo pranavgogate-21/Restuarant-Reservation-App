e109559a6b41c071013cf8c564c10bea26a45f93
pranavgogate-21 <pranavgogate21@gmail.com>
2025-01-02 15:56:21 +0530

    new custom validator for password

--- BE/app/Models/user.py
+++ BE/app/Models/user.py
@@ -1,12 +1,21 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, AfterValidator
+import re
 
+from typing_extensions import Annotated
+
+
+def validate_password(password: str):
+    if re.search(r'[A-Za-z]', password):
+        if re.search(r'\d', password):
+            return password
+    raise ValueError(f"{password} is weak")
 
 
 class User(BaseModel):
-    first_name:str
-    last_name:str
-    email:str
-    phone_number:str
+    first_name:str | None = None
+    last_name:str | None = None
+    email:str | None = None
+    phone_number:str | None = None
 
     class Config:
         from_attributes = True
@@ -15,15 +24,15 @@ class UserIn(BaseModel):
     first_name: str = Field(max_length=15)
     last_name: str = Field(max_length=15)
     email: str = Field(pattern=r'^[^@]+@[^@]+[^@]+\.com$')
-    password: str = Field(min_length=5, pattern=r'^[A-Za-z\d]{5,}$')
+    password: Annotated[str, Field(min_length=5, pattern=r'^[A-Za-z\d]{5,}$'), AfterValidator(validate_password)]
     phone_number: str = Field(min_length=10, max_length=10, pattern=r'^\d+$')
 
-class UserOut(BaseModel):
+class UserUpdate(BaseModel):
     first_name: str | None = None
     last_name: str | None = None
-    email: str | None = None
-    phone_number: str | None = None
+    email: str | None = Field(pattern=r'^[^@]+@[^@]+[^@]+\.com$')
+    phone_number: str | None = Field(min_length=10, max_length=10, pattern=r'^\d+$')
 
 class UserPassword(BaseModel):
     old_password: str
-    new_password: str = Field(min_length=5, pattern=r'^[A-Za-z\d]{5,}$')
+    new_password: Annotated[str, Field(min_length=5, pattern=r'^[A-Za-z\d]{5,}$'), AfterValidator(validate_password)]
