97c666cc3a7eea215f2069b7069c615f1464deb1
pranavgogate-21 <pranavgogate21@gmail.com>
2025-07-18 00:34:37 +0530

    adding dashobard, login components and route modules

--- FE/resto-book/src/app/app.routes.ts
+++ FE/resto-book/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
